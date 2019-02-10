505b86d5a4c39d8cfea3f3013db2f7ef8f220b1a
Anonymous <Anonymous@Anonymous>
2019-02-10 16:57:19 -0600

    compared runtime in TACC nodes

--- +utility/na.m
+++ +utility/na.m
@@ -5,14 +5,19 @@ function ret = na(p, fun, default_val)
 % E.g. cellfun(@(p)utility.na(p, @(q)q.total_duration), data) will return 0
 % where the lambda will throw.
 
+if nargin < 3
+    default_val = 0;
+end
+
+ret = default_val;
+
+if isempty(p)
+    return;
+end
+
 try
     ret = fun(p);
 catch
-    if nargin < 3
-        default_val = 0;
-    end
-    
-    ret = default_val;
 end
 end
 
