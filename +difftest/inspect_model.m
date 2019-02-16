5dcb9fc6c701604a577f089f3100ac2b75a7f1d0
Anonymous <Anonymous@Anonymous>
2019-02-16 09:18:21 -0600

    inspection for difftest

---
+++ +difftest/inspect_model.m
@@ -0,0 +1,20 @@
+function inspect_model(difftest_ob)
+%INSPECT_MODEL Open up models from a difftest object
+%   Detailed explanation goes here
+l = logging.getLogger('inspect_models');
+
+% cellfun(@(p)utility.d(@()open_system([p.loc filesep p.sys '_' difftest.cfg.PRE_EXEC_SUFFIX])),...
+%     difftest_ob.executions);
+
+
+l.info('--Comp Diffs--');
+
+c_d_keys = difftest_ob.comp_diffs.keys();
+for i=1:numel(c_d_keys)
+    k = c_d_keys{i};
+    l.info('%s', k);
+    disp( difftest_ob.comp_diffs(k));
+end
+
+end
+
